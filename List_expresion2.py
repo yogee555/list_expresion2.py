0d44a7c758f2c6a816fa678c396b51297ba9ff99
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-02-11 11:42:05 +0530

    Add files via upload

---
+++ List_expresion2.py
@@ -0,0 +1,12 @@
+# List Slice
+list1 = range(1,6)
+print(list1)
+
+# List no to take
+list1 = [1,2,3,4,5,6,7,8,9,10]
+print(list1[1:])
+print(list1[:1])
+print(list1[2:5])
+print(list1[:6])
+print(list1[1:2:4])
+print(list1[1:8:2])
